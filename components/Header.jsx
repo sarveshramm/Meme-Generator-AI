e900b3077c064990321df5fc8c130384549da9ac
sarveshram A <kit27.am47@gmail.com>
2025-12-09 13:55:48 +0530

    Add files via upload

---
+++ components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+    return (
+        <header>
+            <div className="logo">
+                <img src="/images/troll-face.png" />
+                <h1>Meme Generator</h1>
+            </div>
+            <p>React Course - Project 3</p>
+        </header>
+    )
+}
